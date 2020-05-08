bb7b34783cf1dd8b4b54c0cca13ed90215181e9b
Mattheus Einloft <mattheuseinloft@gmail.com>
2020-05-08 18:51:16 -0300

    adjusting dotenv

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// import { API_URL } from 'react-native-dotenv';
+import { API_URL } from 'react-native-dotenv';
 
 const api = axios.create({
-  baseURL: `http://localhost:3333`,
+  baseURL: `${API_URL}`,
 });
 
 export default api;
